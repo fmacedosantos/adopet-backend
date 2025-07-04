9dd7394c7acad10e5f16b5ba443b5afb6cd2f31a
fmacedosantos <felipemacedos986@gmail.com>
2025-07-04 19:05:49 -0300

    Making our first test

---
+++ src/test/java/br/com/alura/adopet/api/service/CalculadoraProbabilidadeAdocaoTest.java
@@ -0,0 +1,38 @@
+package br.com.alura.adopet.api.service;
+
+import br.com.alura.adopet.api.dto.CadastroAbrigoDTO;
+import br.com.alura.adopet.api.dto.CadastroPetDTO;
+import br.com.alura.adopet.api.model.Abrigo;
+import br.com.alura.adopet.api.model.Pet;
+import br.com.alura.adopet.api.model.ProbabilidadeAdocao;
+import br.com.alura.adopet.api.model.TipoPet;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculadoraProbabilidadeAdocaoTest {
+
+    @Test
+    void cenario01() {
+        // idade 4 e 4kg - ALTA
+
+        Abrigo abrigo = new Abrigo(new CadastroAbrigoDTO(
+                "Abrigo feliz",
+                "94999999999",
+                "abrigofeliz@email.com.br"
+        ));
+        Pet pet = new Pet(new CadastroPetDTO(
+                TipoPet.GATO,
+                "Miau",
+                "Siames",
+                4,
+                "Cinza",
+                4.0f
+        ), abrigo);
+
+        CalculadoraProbabilidadeAdocao calculadora = new CalculadoraProbabilidadeAdocao();
+        ProbabilidadeAdocao probabilidade = calculadora.calcular(pet);
+
+        assertEquals(ProbabilidadeAdocao.ALTA, probabilidade);
+    }
+}
