4f13a09c36c88de4a00124236f9a1eeb4b00ba9d
fmacedosantos <felipemacedos986@gmail.com>
2025-07-04 17:54:49 -0300

    Updating classes to keep up with the course

--- src/main/java/br/com/alura/adopet/api/service/EmailService.java
+++ src/main/java/br/com/alura/adopet/api/service/EmailService.java
@@ -1,24 +1,7 @@
 package br.com.alura.adopet.api.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Service;
+public interface EmailService {
 
-@Service
-public class EmailService {
+    void enviarEmail(String to, String subject, String message);
 
-    @Autowired
-    private JavaMailSender emailSender;
-
-    public void enviarEmail(String to, String subject, String message) {
-        SimpleMailMessage email = new SimpleMailMessage();
-
-        email.setFrom("email@gmail.com");
-        email.setTo(to);
-        email.setSubject(subject);
-        email.setText(message);
-
-        emailSender.send(email);
-    }
 }
