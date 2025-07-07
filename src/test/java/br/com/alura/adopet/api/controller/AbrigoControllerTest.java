2692aee8ee36dfa2641124886613cca1d5e370ca
fmacedosantos <felipemacedos986@gmail.com>
2025-07-06 21:55:06 -0300

    Testing controllers

---
+++ src/test/java/br/com/alura/adopet/api/controller/AbrigoControllerTest.java
@@ -0,0 +1,44 @@
+package br.com.alura.adopet.api.controller;//Código omitido
+
+import br.com.alura.adopet.api.model.Abrigo;
+import br.com.alura.adopet.api.service.AbrigoService;
+import br.com.alura.adopet.api.service.PetService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AbrigoControllerTest {
+
+    @MockBean
+    private AbrigoService abrigoService;
+
+    @MockBean
+    private PetService petService;
+
+    @Mock
+    private Abrigo abrigo;
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void deveriaDevolverCodigo200ParaRequisicaoDeListarAbrigos() throws Exception {
+        //ACT
+        MockHttpServletResponse response = mockMvc.perform(
+                get("/abrigos")
+        ).andReturn().getResponse();
+
+        //ASSERT
+        assertEquals(200, response.getStatus());
+    }
+}
